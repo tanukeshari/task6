e61498badab6ef466f35d73f7c86d5782c688350
tanukeshari <kesharitanu@gmail.com>
2022-11-11 14:21:41 +0530

    Master

---
+++ dom.js
@@ -0,0 +1,23 @@
+// console.log(document.URL);
+// console.log(document.body);
+
+// var headerTitle = document.getElementById('header-title');
+// headerTitle.textContent='tanu';
+// headerTitle.innerText='keshari';
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// var header =document.getElementsByClassName('main-header');
+// headerTitle.style.borderBottom-'solid 30px yellow';
+// console.log(headerTitle.style.borderBottom);
+
+//QUERYSELECTOR //
+
+// question 1
+
+var seconditem= document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.backgroundColor='green';
+
+//question 2
+
+var thirditem= document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.color='white';
